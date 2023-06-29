22ef5821eb6594afaebd7597c2109d66d77fef57
Roshanbalnadu <roshanbalnadu@gmail.com>
2023-06-29 21:18:59 +0530

    repo

--- src/main/java/com/city/demo/repository/CityRepository.java
+++ src/main/java/com/city/demo/repository/CityRepository.java
@@ -4,6 +4,9 @@ import com.city.demo.model.CityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CityRepository extends JpaRepository<CityEntity,Integer>  {
+    List<CityEntity> findByname(String name);
 }
